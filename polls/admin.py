57505edcdfbce784a9c205e39405f7ac83a12483
bbbyung2 <bhko0524@naver.com>
2020-01-23 03:33:28 +0900

    djangopolls

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)  # admin page에서 app 보이게
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)  # admin page에서 app 보이게
 
